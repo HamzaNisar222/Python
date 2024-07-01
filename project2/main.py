daaa5c93b3f950163537c9b3a4a2af3a1360d704
HamzaNisar222 <hamzanisar941@gmail.com>
2024-07-01 18:02:16 +0500

    Python Initial

---
+++ project2/main.py
@@ -0,0 +1,5 @@
+import time
+
+while True:
+    print("Project 2 is running...")
+    time.sleep(10)
---
+++ project3/script.py
@@ -0,0 +1,5 @@
+import time
+
+while True:
+    print("Project 3 is running...")
+    time.sleep(10)
