769c0266dc281a99a9cdf8f39ed3e26bc5274a87
HamzaNisar222 <168709266+HamzaNisar222@users.noreply.github.com>
2024-07-04 05:39:18 +0500

    Delete BroCode/*args.py

--- BroCode/*args.py
+++
@@ -1,9 +0,0 @@
-# *args are used to accept multiple positional arrguments as a tuple in the function 
-
-def add(*args):
-    sum=0
-    for i in args:
-        sum+=i
-    return sum 
-
-add(1,2,3,4,5)   
